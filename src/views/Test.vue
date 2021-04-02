dcabf95271c2096288c50acae13480fc3cd6e53e
Daniel <danielanugr@yahoo.co.id>
2021-04-02 20:02:31 +0700

    added radio button with custom value

---
+++ src/views/Test.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <form action="" method="post">
+      <RadioButton :label="label" :items="items"></RadioButton>
+    </form>
+  </div>
+</template>
+
+<script>
+import RadioButton from '../components/RadioButton'
+
+export default {
+  name: 'Test',
+  components: {
+    RadioButton
+  },
+  data () {
+    return {
+      label: 'Name',
+      items: [
+        {
+          value: 1,
+          name: 'radio 1'
+        },
+        {
+          value: 2,
+          name: 'radio 2'
+        },
+        {
+          name: 'radio 3',
+          type: 'date'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style></style>
